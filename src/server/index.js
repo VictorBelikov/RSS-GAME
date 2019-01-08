db5c686feafb5cba509a06d3ae3c875c2cee1b30
V1ctoR <v1ctor.belikov@gmail.com>
2019-01-08 16:37:17 +0300

    fix: fix server functionality

--- src/server/index.js
+++ src/server/index.js
@@ -5,4 +5,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-mongoose.connect('');
+app.listen(process.env.PORT);
+
+mongoose.connect('mongodb://rss-game-user:1111111831victor@ds251804.mlab.com:51804/rss-game');
+const Player = mongoose.model('Player', { name: String, kills: Number });
+const pl1 = new Player({ name: 'Test_player_1', kills: 1 });
+pl1.save().then(() => console.log('saved'));
